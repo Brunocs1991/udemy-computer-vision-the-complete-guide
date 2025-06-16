a5602a3fd5486ea70ac8a6630a7f09df3009e489
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-06-16 19:54:24 -0300

    Detecção de faces com haarcascade e OpenCV

--- app/01_face_detection/opencv.py
+++ app/01_face_detection/opencv.py
@@ -12,7 +12,7 @@ import cv2  # OpenCV
 from utils.image_utils import show_image
 
 # %%
-img = cv2.imread("../data/people1.jpg")
+img = cv2.imread("../data/images/people1.jpg")
 # %%
 img.shape
 
@@ -33,3 +33,18 @@ gray_img.shape
 show_image(gray_img, "People 1 Gray")
 
  # %%
+ ## face detection
+face_detector = cv2.CascadeClassifier("../data/cascade/haarcascade_frontalface_default.xml")
+
+# %%
+detections = face_detector.detectMultiScale(gray_img)
+detections
+
+# %%
+len(detections)
+
+# %%
+for (x, y, w, h) in detections:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+show_image(img, "People 1 with detections")
+# %%
